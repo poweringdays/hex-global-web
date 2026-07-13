2423d5632eda325d600ca87d501a2f77c6ca2687
poweringdays <160748893+poweringdays@users.noreply.github.com>
2026-07-13 13:26:42 +0000

    Delete docs/assets directory

--- docs/assets/main-CjBtK3EO.js
+++
@@ -1,1 +0,0 @@
-import{b as s}from"./main-DTeH1Pf4.js";window.logs_table=[];async function t(){for(;window.logs_table.length===0;){await new Promise(l=>setTimeout(l,500));const o=await s();window.logs_table=o}}t();
